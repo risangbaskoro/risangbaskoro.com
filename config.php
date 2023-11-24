d4463d90eccdd773fe287adbaa0b753b8069850e
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-24 23:31:47 +0700

    Init Jigsaw

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Risang Baskoro',
+    'description' => 'Personal Blog.',
+    'collections' => [],
+];
---
+++ source/index.blade.php
@@ -0,0 +1,9 @@
+@extends('_layouts.main')
+
+@section('body')
+<div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold">
+        Hi there! 👋
+    </h1>
+</div>
+@endsection
