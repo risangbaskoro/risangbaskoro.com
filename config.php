9ba5941696dcc25ec3eb70069b538ad17c9635b4
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-26 15:19:01 +0700

    Init

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // Main Site Config
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Risang Baskoro',
+    'description' => 'Personal Site',
+
+    // Collections
+    'collections' => [
+
+    ],
+];
---
+++ source/_layouts/main.blade.php
@@ -0,0 +1,6 @@
+@extends('_layouts.master')
+
+@push('meta')
+    <meta name="description" content="{{ $page->description }}">
+    <title>{{ $page->title }}</title>
+@endpush
