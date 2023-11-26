fdc45fddbdfbfbdc1cf61e4e16907da774aaaa15
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-26 23:21:51 +0700

    Disable clicking in nav if current page

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="sticky z-50 top-0 backdrop-blur-xl">
+<header class="sticky z-50 top-0 backdrop-blur-xl hover:bg-stone-900 transition">
     <nav class="absolute p-6 grid grid-cols-3 justify-between items-center w-full bg-stone-900/75">
         <a href="/">
             <img src="https://github.com/risangbaskoro.png?size=50" alt="Logo" draggable="false">
@@ -7,7 +7,10 @@
         <div class="flex space-x-4 justify-center items-center">
             @foreach($page->navigations as $nav)
                 <a href="{{ $page->baseUrl . $nav->link }}"
-                   class="px-4 py-2 hover:bg-stone-700 transition duration-300 ease-in-out rounded-lg hover:shadow !shadow-stone-600"
+                   @class([
+                        "px-4 py-2 hover:bg-stone-700 transition duration-300 ease-in-out rounded-lg hover:shadow !shadow-stone-600",
+                        "pointer-events-none" => $page->getUrl() === $page->baseUrl . $nav->link,
+                    ])
                 >
                     {{ $nav->title }}
                 </a>
