35abf6b87f422edc85148fd48cc9c193b2087b58
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-26 22:56:36 +0700

    nav tag

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,9 +1,9 @@
-<div class="p-6 grid grid-cols-3 justify-between items-center w-full">
+<nav class="p-6 grid grid-cols-3 justify-between items-center w-full">
     <a href="/">
         <img src="https://github.com/risangbaskoro.png?size=50" alt="Logo" draggable="false">
     </a>
 
-    <nav class="flex space-x-4 justify-center items-center">
+    <div class="flex space-x-4 justify-center items-center">
         @foreach($page->navigations as $nav)
             <a href="{{ $page->baseUrl . $nav->link }}"
                class="px-4 py-2 hover:bg-stone-700 transition duration-300 ease-in-out rounded-lg hover:shadow !shadow-stone-600"
@@ -11,5 +11,5 @@
                 {{ $nav->title }}
             </a>
         @endforeach
-    </nav>
-</div>
+    </div>
+</nav>
