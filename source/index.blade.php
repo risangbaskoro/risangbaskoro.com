f7cfa5b3a2bc65e14720ffb7ca4f5dc965678e59
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-26 17:04:27 +0700

    Posts index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,13 +3,5 @@
 @section('body')
     <div class="p-8">
         <h1 class="text-3xl font-bold">Hello world!</h1>
-
-        <ul>
-            @foreach($posts as $post)
-                <li>
-                    <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
-                </li>
-            @endforeach
-        </ul>
     </div>
 @endsection
---
+++ source/posts.blade.php
@@ -0,0 +1,17 @@
+@extends('_layouts.main')
+
+@section('body')
+    <div class="p-8">
+        <h1 class="text-3xl font-bold">
+            Posts
+        </h1>
+
+        <ul>
+            @foreach($posts as $post)
+                <li>
+                    <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@endsection
