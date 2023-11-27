507afc56ddf1640a23459d93577f87a2a046d30e
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-27 07:53:41 +0700

    Google Analytics

---
+++ source/_partials/google-analytics.blade.php
@@ -0,0 +1,12 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1385D6FPRF"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-1385D6FPRF');
+</script>
