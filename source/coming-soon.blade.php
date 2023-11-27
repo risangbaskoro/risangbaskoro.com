2d9e89a894f2640da425437d62a16e4d60e2c683
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-28 02:25:13 +0700

    Coming soon page

---
+++ source/coming-soon.blade.php
@@ -0,0 +1,10 @@
+@extends('_layouts.master')
+
+@section('body')
+    @include('_partials.header')
+    <div class="container content-container prose prose-invert">
+        <h1>Coming Soon</h1>
+        <p>Stay tuned for more updates!</p>
+    </div>
+    @include('_partials.footer')
+@endsection
