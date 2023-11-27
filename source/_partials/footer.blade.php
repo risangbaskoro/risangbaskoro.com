c41c6eba4c7847ea94a2a5fe44b1bddf9e5ccb7c
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-28 03:56:17 +0700

    Update footer

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,14 +1,20 @@
-<footer class="mx-auto w-full p-8 flex flex-col justify-center items-center space-y-2 text-sm text-center">
-    <p>
-        © {{ date('Y') }} {{ $page->siteName }}.
-        •
-        <a href="{{ $page->baseUrl . '/privacy-policy' }}" class="font-bold hover:underline">Privacy policy</a>
-    </p>
-    <p>
-        Built with
-        <a href="https://jigsaw.tighten.co" target="_blank" class="font-bold hover:underline">Jigsaw</a> and
-        <a href="https://tailwindcss.com" target="_blank" class="font-bold hover:underline">TailwindCSS</a>.
+<footer class="mx-auto w-full p-8 text-sm text-center">
+    <div class="flex flex-col justify-center items-center gap-2">
+        <p>
+            © {{ date('Y') }} {{ $page->siteName }}
+        </p>
+        <p>
+            <a href="{{ $page->baseUrl . '/privacy-policy' }}" class="font-bold hover:underline">
+                Privacy policy
+            </a>
+        </p>
+        <p class="mt-4">
+            Built with
+            <a href="https://jigsaw.tighten.co" target="_blank" class="font-bold hover:underline">Jigsaw</a> and
+            <a href="https://tailwindcss.com" target="_blank" class="font-bold hover:underline">TailwindCSS</a>.
 
-        Code highlighting provided by <a href="https://torchlight.dev" target="_blank" class="font-bold hover:underline">Torchlight</a>.
-    </p>
+            Code highlighting provided by <a href="https://torchlight.dev" target="_blank"
+                                             class="font-bold hover:underline">Torchlight</a>.
+        </p>
+    </div>
 </footer>
