d800e79042355c65df6b6d064c3d6380ae730aaa
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-29 04:50:28 +0700

    Homepage hero appear animation

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -4,3 +4,17 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(
     ScrollTrigger,
 );
+
+const homepageHeroAppearTimeline = gsap.timeline();
+
+homepageHeroAppearTimeline
+    .from('#hero-title', {
+        opacity: 0,
+        y: 100,
+        ease: 'power1.out',
+    })
+    .from('#hero-content', {
+        opacity: 0,
+        y: 100,
+        ease: 'power1.out',
+    });
