d36044f9e118ac36803215f77b0425c258dbccd7
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-29 04:34:46 +0700

    New Homepage

---
+++ source/_partials/homepage/hero.blade.php
@@ -0,0 +1,18 @@
+<section id="hero" class="h-screen">
+    <div id="hero-container" class="container mx-auto h-full flex justify-center items-center">
+        <div id="hero-prose-container" class="container-content prose prose-invert prose-sm md:prose-lg lg:prose-2xl prose-p:text-justify prose-p:md:text-left">
+            <h1 id="hero-title" class="text-primary-400 font-extralight font-serif">
+                From
+                <em>Hobbyist</em>
+                to
+                <em class="text-primary-50 bg-gradient-to-r from-primary-900 to-50% to-primary-950 rounded px-2">Professional</em>
+                Web Application Developer
+            </h1>
+            <p id="hero-content">
+                In today's dynamic digital landscape, a robust online presence is crucial for businesses.
+                With expertise in PHP and Laravel, I specialize in building and consulting on top-tier web applications.
+                My mission is to empower clients by solving their unique challenges and delivering exceptional results.
+            </p>
+        </div>
+    </div>
+</section>
