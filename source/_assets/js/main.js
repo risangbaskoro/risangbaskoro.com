ccf22382b5c453478c6abea92120b16ec1ab1d18
Risang Baskoro <risangbaskoro@gmail.com>
2023-11-29 04:58:16 +0700

    Add scroll prompt to hero

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -17,4 +17,7 @@ homepageHeroAppearTimeline
         opacity: 0,
         y: 100,
         ease: 'power1.out',
+    })
+    .from('#hero-scroll-prompt', {
+        opacity: 0,
     });
--- source/_partials/homepage/hero.blade.php
+++ source/_partials/homepage/hero.blade.php
@@ -1,5 +1,5 @@
-<section id="hero" class="h-screen">
-    <div id="hero-container" class="container mx-auto h-full flex justify-center items-center">
+<section id="hero" class="h-screen relative">
+    <div id="hero-container" class="container mx-auto h-full flex flex-col justify-center items-center">
         <div id="hero-prose-container" class="container-content prose prose-invert prose-sm md:prose-lg lg:prose-2xl prose-p:text-justify prose-p:md:text-left">
             <h1 id="hero-title" class="text-primary-400 font-extralight font-serif">
                 From
@@ -13,6 +13,9 @@
                 With expertise in PHP and Laravel, I specialize in building and consulting on top-tier web applications.
                 My mission is to empower clients by solving their unique challenges and delivering exceptional results.
             </p>
+        </div>
+        <div id="hero-scroll-prompt" class="absolute bottom-4 animate-bounce text-xl lg:text-4xl">
+            &darr;
         </div>
     </div>
 </section>
